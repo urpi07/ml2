5c1d1ed1c1396ed78543fe0565eedf2f96863cba
marcelo <urpianojr07@yahoo.com>
2018-02-22 20:05:30 +0800

    Added genderlist model

---
+++ application/models/GenderList.php
@@ -0,0 +1,30 @@
+<?php
+class Email extends RestFul_Model{
+
+	public function __construct(){
+		parent::__construct("ml_genderlist");			
+	}
+	
+	public function add($data){			
+		$parent::setData($data);
+		return  $parent::add();			
+	}
+	
+	public function edit($data){
+		$parent::setData($data);
+		return $parent::edit($data);
+	}
+	
+	public function delete($id){
+		return $parent::delete($id)
+	}
+	
+	public function get($id){
+		return $parent::get($id);
+	}
+	
+	public function getLists(){		
+		$query = $this->db->query('Select * From ml_genderlist');
+		return $query->result();
+	}
+}
